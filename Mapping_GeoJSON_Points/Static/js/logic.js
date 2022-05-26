2fbcd4bea2b8a4b6145bc52f27c4d7f1a3c35e68
lalita-ponnapalli <lalita.pn1@gmail.com>
2022-05-26 14:33:36 -0500

    new changes

--- Mapping_GeoJSON_Points/Static/js/logic.js
+++ Mapping_GeoJSON_Points/Static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 // Create the map object with a center and zoom level.
 
-let map = L.map('mapid').setView([30, 30], 2);
+//let map = L.map('mapid').setView([30, 30], 2);
 // {
 //     center: [
 //       40.7, -94.5
@@ -38,9 +38,9 @@ let map = L.map('mapid').setView([30, 30], 2);
 //       console.log(layer);
 //       layer.bindPopup();
       
-    }
+//   }
 
-  }).addTo(map);;
+//  }).addTo(map);;
 // let line = [
 //   [33.9416, -118.4085],  
 //   [37.6213, -122.3790],
@@ -65,15 +65,36 @@ let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/t
         maxZoom: 18,
         accessToken: API_KEY
     });
+// We create the dark view tile layer that will be an option for our map.
+let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    accessToken: API_KEY
+});
+// Create a base layer that holds both maps.
+let baseMaps = {
+    Street: streets,
+    Dark: dark
+  };
+// Create the map object with center, zoom level and default layer.
+let map = L.map('mapid', {
+    center: [30, 30],
+    zoom: 2,
+    layers: [streets]
+})
+// Pass our map layers into our layers control and add the layers control to the map.
+L.control.layers(baseMaps).addTo(map);
  
     // Then we add our 'graymap' tile layer to the map.
     streets.addTo(map);
-    let airportData = "https://raw.githubusercontent.com/https://github.com/Lalita-ponnapalli/Mapping_Earthquakes.git/Mapping_Earthquakes/main/majorAirports.json";
+let airportData = "https://raw.githubusercontent.com/Lalita-ponnapalli/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
 // Grabbing our GeoJSON data.
-    d3.json(airportData).then(function(data) {
-        console.log(data);
-    // Creating a GeoJSON layer with the retrieved data.
-    L.geoJSON(data).addTo(map);
+d3.json(airportData).then(function(data) {
+    console.log(data);
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data,onEachFeature: function (feature, layer) {
+    layer.bindPopup('<h1>'+feature.properties.f1+'</h1><p>name: '+feature.properties.f2+'</p>');
+  }).addTo(map);
 });
    
     
