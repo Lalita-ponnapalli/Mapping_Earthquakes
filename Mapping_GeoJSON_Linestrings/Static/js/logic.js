f3942a9b59283eb69372fae7b5a21f1e650e90bd
lalita-ponnapalli <lalita.pn1@gmail.com>
2022-05-27 17:39:19 -0500

    message

--- Mapping_GeoJSON_Linestrings/Static/js/logic.js
+++ Mapping_GeoJSON_Linestrings/Static/js/logic.js
@@ -11,12 +11,7 @@ let satelliteStreets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/sate
         maxZoom: 18,
         accessToken: API_KEY
     });
-// We create the dark view tile layer that will be an option for our map.
-// let dark = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-// attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
-//     maxZoom: 18,
-//     accessToken: API_KEY
-// });
+
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
@@ -29,23 +24,16 @@ let map = L.map('mapid', {
     zoom: 2,
     layers: [satelliteStreets]
 });
+
 // Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/Lalita-ponnapalli/Mapping_Earthquakes/main/torontoNeighborhoods.json
-// Pass our map layers into our layers control and add the layers control to the map.
-//L.control.layers(baseMaps).addTo(map);
- 
-    // Then we add our 'graymap' tile layer to the map.
-    //streets.addTo(map);
-//let airportData = "https://raw.githubusercontent.com/Lalita-ponnapalli/Mapping_Earthquakes/Mapping_GeoJSON_Points/majorAirports.json"
-// Accessing the Toronto airline routes GeoJSON URL.
-//let torontoData = "https://raw.githubusercontent.com/<GitHub_name>/Mapping_Earthquakes/main/torontoRoutes.json";
-// let torontoData="https://raw.githubusercontent.com/Lalita-ponnapalli/Mapping_Earthquakes/Mapping_GeoJSON_Linestrings/Mapping_GeoJSON_Linestrings/Static/js/logic.js"
-// // Grabbing our GeoJSON data.
-// // Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-    console.log(data);
-  // Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data).addTo(map);
-});
+let torontoHoods = "https://raw.githubusercontent.com/Lalita-ponnapalli/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 L.control.layers(baseMaps).addTo(map);
+   
+d3.json(torontoHoods).then(function(data) {
+
+    console.log(data);
+    // Creating a GeoJSON layer with the retrieved data.
+    L.geoJSON(data).addTo(map);
+});
+//L.control.layers(baseMaps).addTo(map);
    
